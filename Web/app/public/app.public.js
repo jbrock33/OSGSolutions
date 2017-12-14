f82258f4363465f19f45324b2a2eb62a695269ad
wabba <wabba@LAPTOP-FT4Q9Q0J>
2017-12-14 11:45:56 -0800

    base site skeleton functioning.  ui-router working.

---
+++ Web/app/public/app.public.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    'use strict';
+    window.APP = window.APP || {};
+    APP.NAME = "publicApp";
+    angular
+        .module(APP.NAME, ['ui.router', APP.NAME + '.routes', 'ngCookies', 'ngRoute']);
+
+})();
